9fbf4fb1ac13908879922562d86fc58fc22f91e8
Philipp Fischer <philipp.fischer@js-soft.com>
2024-10-17 16:59:13 +0200

    updated tests

--- src/test_default.py
+++ src/test_default.py
@@ -2,23 +2,20 @@ import src
 import numpy as np
 
 
-def test_foo():
-    assert True
-
 def test_forward():
     x = np.random.uniform(0, 10, 20)
 
-    FF = src.FeedForward(20, 1024, 5)
-    out = FF(x)
+    ff = src.FeedForward(20, 1024, 5)
+    out = ff(x)
 
-    assert len(out) == 5
-    assert sum(np.round(out, 9)) == 1
+    assert out.shape == (5,)
+    assert np.isclose(sum(out), 1)
 
 def test_2dim_forward():
     x = np.random.uniform(0, 10, (3, 20))
 
-    FF = src.FeedForward(20, 1024, 5)
-    out = FF(x)
+    ff = src.FeedForward(20, 1024, 5)
+    out = ff(x)
 
     assert out.shape == (3, 5)
-    assert sum(sum(np.round(out, 9))) == 1
+    assert np.isclose(sum(sum(out)), 1)
